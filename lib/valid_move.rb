264ad4770fa40fa0086a042b22ff0fcc06de6c4d
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-04-19 19:23:38 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,10 @@ def valid_move?(board, index)
   elsif board[0] == " " || board[0] == "" || board[0] == nil
     true
   elsif board[0] == "X" || board[0] == "O"
-    nil || false
+     false
+   elsif 
+     nil
+     
   end
 end
 
