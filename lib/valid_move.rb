e77a928869a903427ca7ef07105ead2aa84450ca
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-04-19 19:55:22 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,17 +1,17 @@
-# code your #valid_move? method here
-.. def valid_move?(board, index)
+# code your #valid_move? method here 
+.. def valid_move?(board, index) 
 ..   if board[0] == be_between?(0,8) && !position_taken?(board, index) == false
-..     true
-..
-..     end
-.. end
-..
-..
-.. # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-.. def position_taken?(board, index)
-..   if board[0] == " " || board[0] == "" || board[0] == nil
-..     false
-..   elsif board[0] == "X" || board[0] == "O"
-..     true
-..   end
+..      return true
+..        
+..     end 
+.. end 
+..  
+..  
+.. # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above. 
+.. def position_taken?(board, index) 
+..   if board[0] == " " || board[0] == "" || board[0] == nil 
+..     return false 
+..   elsif board[0] == "X" || board[0] == "O" 
+..     return true 
+..   end 
 ..  end
